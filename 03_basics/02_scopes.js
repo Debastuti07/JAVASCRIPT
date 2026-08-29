9b105e5257a20418db81164f0a19a4aaf7ff43d2
Debastuti07 <debastutide@gmail.com>
2026-08-30 01:49:40 +0530

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -10,11 +10,51 @@ if(true){
 //    var c=30;
 
 }
-for(let i=0;i<array.length;i++)
-{
-    const element =array[i];
-}
+
 
 console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+     const username="tuti"
+
+     function two(){
+        const website="youtube"
+        console.log(username);
+        
+     }
+//    console.log(website);
+   
+     two();
+
+}
+one();
+
+if(true){
+    const username="megha";
+    if(username==="megha"){
+        const website=" youtube";
+        console.log((username + website));
+        
+    }
+    // console.log(website);//error
+    
+}
+// console.log(username);//error
+
+//++++++++++++++++++ interesting +++++++++++++++++++++++++
+
+
+function addone(num){
+    return num +1;
+}
+console.log(addone(5));
+
+
+const addTwo=function(num)
+{
+     return num+2;
+}
+console.log(addTwo(8));
