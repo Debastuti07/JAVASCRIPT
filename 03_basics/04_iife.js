48791ebc57ab50469265d1eb6028f7d7331dc5cf
Debastuti07 <debastutide@gmail.com>
2026-08-30 12:01:04 +0530

    IIFE

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,15 @@
+//Immediately Invoked Fuction Expressions(IIFE)
+
+(function chai(){
+    //named IIFE
+    console.log(`DB CONNECTED`);
+    
+})
+();
+
+( (name)=>{
+    //unnamed IIFE
+    console.log(`DB CONNECTED TWO ${name}`);
+    
+})('megha');
+
